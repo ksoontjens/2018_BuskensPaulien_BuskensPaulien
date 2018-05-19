1a0549c1e68a856c86e903756fbdbf8895d61c89
PaulienBuskens <paulien.buskens1@hotmail.com>
2018-05-19 11:25:33 +0200

    add tekst

--- Converter/src/hellotvxlet/HelloTVXlet.java
+++ Converter/src/hellotvxlet/HelloTVXlet.java
@@ -23,7 +23,10 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
     private HScene scene;
     //debuggen activeren of niet
     private boolean debug=true;
-    private HTextButton knop1, knop2;
+    
+    
+    private HTextButton knop1, knop2, knop3, knop4;
+    private HStaticText tekstLabel1;
                 
     //background
     public void notifyRelease(ResourceProxy proxy){}
@@ -41,6 +44,7 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
      public void imageLoadFailed(HBackgroundImageEvent e){
          System.out.println("image kan niet geladen worden.");
      }
+     
         
         public void initXlet(XletContext context)throws XletStateChangeException{
             
@@ -57,6 +61,8 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
             //een instantie van de Scene aanvragen aan de factory.
             scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
             
+            
+            //knoppen
             knop1 = new HTextButton("euro to");
             knop1.setLocation(280,220);
             knop1.setSize(180,50);
@@ -69,8 +75,23 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
             knop2.setBackground(new DVBColor(204,40,40,179));
             knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
             
+            knop3 = new HTextButton("1");
+            knop3.setLocation (100,210);
+            knop3.setSize(50,50);
+            knop3.setBackground(new DVBColor(204,40,40,179));
+            knop3.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            knop4 = new HTextButton("5");
+            knop4.setLocation (160,210);
+            knop4.setSize(50,50);
+            knop4.setBackground(new DVBColor(204,40,40,179));
+            knop4.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            
+            
             knop1.setFocusTraversal(null, knop2, null, null); //op, neer, links, rechts
             knop2.setFocusTraversal(knop1,null,null,null);
+            knop3.setFocusTraversal(null,null,null, knop4);
+            knop4.setFocusTraversal(null,null,knop3,null);
             
             scene.add(knop1);
             scene.add(knop2);
@@ -79,9 +100,22 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
             
             knop1.setActionCommand("knop1_actioned");
             knop2.setActionCommand("knop2_actioned");
+            knop3.setActionCommand("knop3_actioned");
+            knop3.setActionCommand("knop4_actioned");
             
             knop1.addHActionListener(this);
             knop2.addHActionListener(this);
+            knop3.addHActionListener(this);
+            knop4.addHActionListener(this);
+            
+            
+            
+            //tekst vlakken
+            tekstLabel1 = new HStaticText("Euro to");
+            tekstLabel1.setLocation(100,150);
+            tekstLabel1.setSize(280,50);
+            tekstLabel1.setBackground(new DVBColor(204,40,40,179));
+            tekstLabel1.setBackgroundMode(HVisible.BACKGROUND_FILL);
             
             
             //HScreen object opvragen
@@ -122,12 +156,18 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
               if(e.getActionCommand() == "knop1_actioned"){
                 scene.remove(knop1);
                 scene.remove(knop2);
-                System.out.println("Start game");
+                scene.add(knop3);
+                scene.add(knop4);
+                scene.add(tekstLabel1);
+                System.out.println("Euro to");
                 
             }else if(e.getActionCommand()== "knop2_actioned"){
                 scene.remove(knop1);
                 scene.remove(knop2);
-                System.out.println("How To");
+                scene.add(knop3);
+                scene.add(knop4);
+                scene.add(tekstLabel1);
+                System.out.println("To Euro");
                 
             }
         }
